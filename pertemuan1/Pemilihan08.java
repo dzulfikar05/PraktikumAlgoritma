45038b3addd00040cf869f7eb6eb0293e48b8e11
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2024-02-21 14:25:05 +0700

    init

---
+++ pertemuan1/Pemilihan08.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pemilihan08{
+    public static void main(String[] args) {
+        
+        int tugas = 0, kuis = 0, uts = 0, uas = 0; 
+
+        System.out.println("");
+    }
+    
+    
+
+}
