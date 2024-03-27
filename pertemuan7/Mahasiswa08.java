7d9106360204521bf81bf20a9a035a210ab51abf
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2024-03-27 11:20:33 +0700

    percobaan 1 - 4 pertemuan 7

---
+++ pertemuan7/Mahasiswa08.java
@@ -0,0 +1,20 @@
+public class Mahasiswa08 {
+    int nim; 
+    String nama;
+    int umur;
+    double ipk;
+    Mahasiswa08(int ni, String n, int u, double i)
+    {
+        nim = ni;
+        nama = n;
+        umur = u;
+        ipk = i; 
+    }
+
+    void tampil () {
+        System.out.println("Nim = " + nim);
+        System. out.println("Nama = " + nama) ;
+        System. out.println("Umur = " + umur) ;
+        System. out.println("IPK = " + ipk);
+    }
+}
---
+++ pertemuan7/SortMain.java
@@ -0,0 +1,12 @@
+public class SortMain {
+    public static void main(String[] args) {
+        int data[] ={10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("sorting dengan merge sort");
+        MergeSorting08 mSort= new MergeSorting08 ();
+        System. out.println("data awal");
+        mSort.printArray (data);
+        mSort.mergeSort (data) ;
+        System. out.println("setelah diurutkan");
+        mSort.printArray (data) ;
+    }
+}
