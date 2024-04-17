9601731d3e1ce5908032379b4dd2aaa0f6038a26
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2024-04-17 11:38:40 +0700

    add more data

--- uts/Main08.java
+++ uts/Main08.java
@@ -13,6 +13,11 @@ public class Main08 {
         rekening08.tambah(new Rekening08("16240401 2243","Fuller","Finn Dunlap","571-7062","convallis@Vestibulumanteipsum.org"));
         rekening08.tambah(new Rekening08("16270525 0112","Malcolm","Keane Floyd","623-0234","porttitor.tellus.non@Curabitur.ca"));
         rekening08.tambah(new Rekening08("16971204 2416","Geoffrey","Stephen Pratt","1-683-416-8323","ut.pellentesque@luctusutpellentesque.com"));
+        rekening08.tambah(new Rekening08("16100727 8862","Rudyard","Charles Morales","650-5379","Proin.eget@velitegestaslacinia.ca"));
+        rekening08.tambah(new Rekening08("16460329 4259","Troy","Damon Guerra","897-7608","pede.Suspendisse.dui@a.ca"));
+        rekening08.tambah(new Rekening08("16320421 3437","Alec","Cooper Lee","792-4447","non@mus.com"));
+        rekening08.tambah(new Rekening08("16180729 7229","Walter","Seth Drake","863-8209","Pellentesque.ut.ipsum@neque.ca"));
+        rekening08.tambah(new Rekening08("16950313 6823","Simon","Burton Gates","592-6919","tellus.justo.sit@commodoauctor.net"));
         
         
         // insert Transaction
@@ -21,6 +26,11 @@ public class Main08 {
         transaksi08.tambah(new Transaksi08(838632,350929,328316,"2021-09-18 23:00:04", "active"));
         transaksi08.tambah(new Transaksi08(230659,204434,690503,"2022-02-02 19:10:34", "active"));
         transaksi08.tambah(new Transaksi08(770592,334245,444267,"2020-08-11 13:36:56", "active"));
+        transaksi08.tambah(new Transaksi08(685302,451002,376442,"2020-05-23 07:34:53", "active"));
+        transaksi08.tambah(new Transaksi08(816129,851403,597842,"2021-07-18 19:41:20", "active"));
+        transaksi08.tambah(new Transaksi08(989609,333823,802752,"2022-02-01 01:13:11", "active"));
+        transaksi08.tambah(new Transaksi08(297103,396116,779589,"2021-07-03 01:09:49", "active"));
+        transaksi08.tambah(new Transaksi08(66190,259150,619774,"2021-09-09 03:57:30", "active"));
       
 
 
